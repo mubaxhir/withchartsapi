87a7459e0cbbff4c7efa8df8563d4025e2b2c029
mubashir <49488919+mubaxhir@users.noreply.github.com>
2020-02-23 17:08:35 +0500

    v0.2.0

--- __main__.py
+++ __main__.py
@@ -1,9 +1,11 @@
 import connexion
+# import sys
 
+# sys.path.append('C:/Dev/Python/withchartsapi_v0.2.0-requirements/api/')
 
 def main():
     app = connexion.FlaskApp(__name__, specification_dir='openapi/')
-    app.add_api('openapi.yaml',validate_responses=True)
+    app.add_api('openapi.yaml')
     app.run(port=8080,debug=True)
 
 if __name__ == '__main__':
